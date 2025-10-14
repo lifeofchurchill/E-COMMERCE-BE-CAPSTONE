59cdac55e0ae7cbfdde5ce21925ae0f2dd567d64
Churchill <reghoyah@gmail.com>
2025-10-14 00:04:51 +0000

    created admin models and registered them so it could be acessed on djangos admin platform

--- ecommerce_api/store/admin.py
+++ ecommerce_api/store/admin.py
@@ -1,18 +1,17 @@
 from django.contrib import admin
 from .models import Category, Product
 
-# Register your models here.
 
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(admin.ModelAdmin): #admin config for Category model
     list_display = ('name',)
 
-admin.site.register(Category, CategoryAdmin)
+admin.site.register(Category, CategoryAdmin) #add Category model to admin site with the above config
 
 
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(admin.ModelAdmin): #admin config for Product model
     list_display = ('name', 'price', 'category', 'created_at', 'created_by', 'stock_quantity')
     list_filter = ('category', 'created_at', 'created_by')
     search_fields = ('name', 'description')
     ordering = ('-created_at',)
 
-admin.site.register(Product, ProductAdmin)
+admin.site.register(Product, ProductAdmin) #add Product model to admin site with the above config
