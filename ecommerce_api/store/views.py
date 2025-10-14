f64fc79e91e4d58739c6fe963a8b2e478315eeae
Churchill <reghoyah@gmail.com>
2025-10-14 23:57:33 +0000

    added django filter backends to enable search filters

--- ecommerce_api/store/views.py
+++ ecommerce_api/store/views.py
@@ -4,6 +4,7 @@ from .models import Category, Product
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 # my Shop views here.
@@ -15,9 +16,9 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all() #fetch all products from the DB
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticatedOrReadOnly] 
-    filter_backends = [SearchFilter] #enable search functionality
+    filter_backends = [DjangoFilterBackend, SearchFilter] #enable filtering and searching
     search_fields = ['name', 'description']
     filterset_fields = ['category'] 
-
+    
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user) #automatically set the created_by field to the current user when a product is created
