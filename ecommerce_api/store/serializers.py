8fb6c2261ddc6fee299d4957e70e1d8ce4a038bf
Churchill <reghoyah@gmail.com>
2025-10-14 00:07:39 +0000

    created a serializers file which would translate to the database from python objects to json and vice versa to be able to communicate with the API

--- ecommerce_api/store/serializers.py
+++ ecommerce_api/store/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Category
+from .models import Category, Product
 
-class CategorySerializer(serializers.ModelSerializer):
+class CategorySerializer(serializers.ModelSerializer): #serializer for the Category model
     class Meta:
         model = Category
         fields = ['id', 'name']
+
+class ProductSerializer(serializers.ModelSerializer): #serializer for the Product model
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'description', 'price', 'category', 'created_at', 'image', 'created_by', 'stock_quantity']
+        read_only_fields = ['id', 'created_at', 'created_by'] #fields that should not be modified directly
