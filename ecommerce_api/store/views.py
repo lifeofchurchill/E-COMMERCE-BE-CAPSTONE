e15adacc4e494bc440cbca6b31571b1e394d4a21
Churchill <reghoyah@gmail.com>
2025-10-14 00:10:51 +0000

    created viewsets for both the produt models and category model

--- ecommerce_api/store/views.py
+++ ecommerce_api/store/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .serializers import CategorySerializer, ProductSerializer
+from .models import Category, Product
+from rest_framework import viewsets
 
-# Create your views here.
+# my Shop views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
