c96ec801893290acc5714ba70df034891c4e7b2b
Churchill <reghoyah@gmail.com>
2025-10-15 23:58:50 +0000

    Added a viewset to be able to get an endpoint for the user creation and also registered it in the urls file

--- ecommerce_api/store/urls.py
+++ ecommerce_api/store/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, ProductViewSet
+from .views import CategoryViewSet, ProductViewSet, UserViewSet
 
 #sets up a router for the API endpoints also gives us a browsable API
 router = DefaultRouter() 
 router.register(r'categories', CategoryViewSet)
 router.register(r'products', ProductViewSet)
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
--- ecommerce_api/store/views.py
+++ ecommerce_api/store/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from .serializers import CategorySerializer, ProductSerializer
+from .serializers import CategorySerializer, ProductSerializer, UserSerializer
 from .models import Category, Product
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.filters import SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from django.contrib.auth.models import User
 
 
 # my Shop views here.
@@ -18,7 +19,11 @@ class ProductViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly] 
     filter_backends = [DjangoFilterBackend, SearchFilter] #enable filtering and searching
     search_fields = ['name', 'description']
-    filterset_fields = ['category'] 
+    filterset_fields = ['category', 'price', 'stock_quantity'] 
     
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user) #automatically set the created_by field to the current user when a product is created
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
