2e901e6f583e7ab96d7bdc061ef384ff4bfd147c
Churchill <reghoyah@gmail.com>
2025-10-19 06:33:09 +0000

    finishing touches on capstone

--- ecommerce_api/store/README.md
+++ ecommerce_api/store/README.md
@@ -1,1 +1,2 @@
 1bd655e757c1bc1120461536aa2488529a543f42 - token for username = eyram
+8ecd8c5001d324560f15a794e7e22eeef6d89ab9 - token for username = Churchill
--- ecommerce_api/store/urls.py
+++ ecommerce_api/store/urls.py
@@ -2,6 +2,9 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import CategoryViewSet, ProductViewSet, UserViewSet
 
+
+
+
 #sets up a router for the API endpoints also gives us a browsable API
 router = DefaultRouter() 
 router.register(r'categories', CategoryViewSet)
@@ -9,5 +12,6 @@ router.register(r'products', ProductViewSet)
 router.register(r'users', UserViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    
 ]
